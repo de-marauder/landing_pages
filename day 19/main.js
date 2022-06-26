e24f7ff3362e1308fa44e761a169bfa8c609d3ce
de-marauder <ezikegodson@gmail.com>
2022-06-26 18:05:46 +0100

    bootstrap cheatsheet

--- day 19/main.js
+++ day 19/main.js
@@ -1,2 +1,14 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+console.log(tooltipTriggerList);
+
+// tooltipTriggerList.forEach((tooltip)=> {
+//     // console.log(tooltip)
+//     tooltip.addEventListener('mouseover', (e)=>{
+//         e.stopPropagation()
+//         e.stopImmediatePropagation()
+//         console.log("You just hovered on a tooltip")
+//     })
+// })
